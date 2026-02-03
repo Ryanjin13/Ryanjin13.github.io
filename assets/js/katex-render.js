2358fe50f4c9da1105d12beb0e85b76291d370f9
Ryanjin13 <145463562+Ryanjin13@users.noreply.github.com>
2026-02-03 11:31:33 +0900

    Add KaTeX config override with strict:false, trust:true

---
+++ assets/js/katex-render.js
@@ -0,0 +1,14 @@
+document.getElementById("katex-render") &&
+  document.getElementById("katex-render").addEventListener("load", () => {
+    renderMathInElement(document.body, {
+      delimiters: [
+        {left: "$$", right: "$$", display: true},
+        {left: "$", right: "$", display: false},
+        {left: "\\(", right: "\\)", display: false},
+        {left: "\\[", right: "\\]", display: true}
+      ],
+      throwOnError: false,
+      strict: false,
+      trust: true
+    });
+  });
